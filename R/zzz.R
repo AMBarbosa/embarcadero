bed709241dd3ac441e63cd093f635a13ef39b65e
A. Marcia Barbosa <ana.marcia.barbosa@gmail.com>
2022-12-28 09:03:53 +0000

    yet one more try...

--- R/zzz.R
+++ R/zzz.R
@@ -2,10 +2,14 @@
 .onLoad <- function(libname, pkgname) {
   setOldClass("bart")
   # setMethod("predict", "bart", predict2.bart)
-  setMethod("predict", c("bart", "RasterStack"), function(object, newdata, ...) { predict2.bart(object, newdata, ...) })
+  setMethod("predict",
+            c("bart", "RasterStack"),
+            function(object, ...) {
+              predict2.bart(object, ...)
+              }
+            )
   setMethod("summary", "bart", summary.bart)
-  # setMethod("predict", "rbart", predict2.bart)
-  setMethod("predict", c("rbart", "RasterStack"), function(object, newdata, ...) { predict2.bart(object, newdata, ...) })
+  setMethod("predict", "rbart", predict2.bart)
   setMethod("summary", "rbart", summary.bart)
   invisible()
 }
