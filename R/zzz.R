216d98e4b59eb63f03f39232c6485dfdc5f3e51f
A. Marcia Barbosa <ana.marcia.barbosa@gmail.com>
2022-12-28 08:46:55 +0000

    setMethod for predict only when 2nd argument is raster

--- R/zzz.R
+++ R/zzz.R
@@ -2,8 +2,10 @@
 .onLoad <- function(libname, pkgname) {
   setOldClass("bart")
   # setMethod("predict", "bart", predict2.bart)
+  setMethod("predict", c("bart", "RasterStack"), function(object, newdata, ...) { predict2.bart(object, newdata, ...) })
   setMethod("summary", "bart", summary.bart)
   # setMethod("predict", "rbart", predict2.bart)
+  setMethod("predict", c("rbart", "RasterStack"), function(object, newdata, ...) { predict2.bart(object, newdata, ...) })
   setMethod("summary", "rbart", summary.bart)
   invisible()
 }
