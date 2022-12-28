e3def39c6553b5d5d21c2c30082819e3ae8c679f
A. Marcia Barbosa <ana.marcia.barbosa@gmail.com>
2022-12-28 12:14:11 +0000

    back to the beginning

--- R/zzz.R
+++ R/zzz.R
@@ -1,13 +1,7 @@
 
 .onLoad <- function(libname, pkgname) {
   setOldClass("bart")
-  # setMethod("predict", "bart", predict2.bart)
-  setMethod("dbarts:::predict.bart",
-            c("bart", "RasterStack"),
-            function(object, newdata) {
-              predict2.bart(object, newdata)
-              }
-            )
+  setMethod("predict", "bart", predict2.bart)
   setMethod("summary", "bart", summary.bart)
   setMethod("predict", "rbart", predict2.bart)
   setMethod("summary", "rbart", summary.bart)
